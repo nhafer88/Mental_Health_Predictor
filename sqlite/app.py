24439f6907fab40bcf84d142ca88e33338814d74
Juan De La Torre <jpdelatorre@ucdavis.edu>
2022-03-03 16:37:13 -0800

    Add files via upload

--- sqlite/app.py
+++ sqlite/app.py
@@ -1,81 +1,90 @@
-import datetime as dt
-import numpy as np
+import dash
+from dash import html,dcc
 import pandas as pd
+import plotly.express as px
+from dash.dependencies import Input, Output
 
-# dependencies to access the sqlite DB
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+#mental csv
+mental_df = pd.read_csv("sqlite_db/mental_update_id_JDLT.csv")
+mental_df = mental_df.drop(columns = "Unnamed: 0")
+mental_df['PK'] = mental_df['city'].str.cat(mental_df['State_ab'],sep=", ")
+mental_states = mental_df['State_name'].unique()
+#mental figure
+fig_mental = px.scatter(mental_df.loc[mental_df["State_name"]== "California"], x="PK", y=["Low_Confidence_Limit", "Data_Value", "High_Confidence_Limit"],title = "Mental Health Score per City")
 
-from flask import Flask, redirect, url_for, render_template, jsonify, request, session, flash
+#population csv
+population_df = pd.read_csv("sqlite_db/city_update_id_JDLT.csv")
+population_df['PK'] = population_df['city'].str.cat(population_df['state_id'],sep=", ")
+population_df = population_df.drop(['Unnamed: 0'], axis = 1)
+population_df = population_df[['PK', 'state_id', 'state_name', 'city', 'population', 'density']]
+population_df = population_df.sort_values('population', ascending=False).drop_duplicates(subset=['population'])
+population_states = population_df['state_name'].unique()
+#population table
+fig_population = px.histogram(population_df.loc[population_df["state_name"]== "California"].\
+    sort_values(by='population', ascending = False).head(20), x='PK', y=['population','density'], barmode='group',\
+        labels={
+            "PK": "City, ST",
+            "sum of value" : "Total"
+        }, title = "Total Population with Calculated Density")
 
-engine = create_engine("sqlite:///sqlite_db/mhp_db.sqlite")
-Base = automap_base()
-Base.prepare(engine, reflect = True)
+#income csv
+income_df = pd.read_csv("sqlite_db/income_update_id_JDLT.csv")
+income_df['PK'] = income_df['City'].str.cat(income_df['State_ab'],sep=", ")
+income_df = income_df.drop(['Unnamed: 0', 'ALand', 'AWater'], axis=1)
+income_df = income_df[income_df.Median != 300000]
+income_df = income_df[['PK','id', 'State_ab', 'State_Name', 'City', 'Median', 'Stdev']]
+income_states = income_df['State_Name'].unique()
+#income figure
+fig_income = px.scatter(income_df.loc[income_df["State_Name"]== "California"], x="Median", y="PK",
+	         size="Stdev", color="State_Name",
+                 hover_name="PK", log_x=True, title = "income figure")
 
-City = Base.classes.city
-Incomes = Base.classes.income
-Mental = Base.classes.mental
+#making the options for the dropdown menu
+state_labels = [{'label':i, 'value':i} for i in mental_states]
 
-session = Session(engine)
+app = dash.Dash()
+app.layout = html.Div([html.Div('MHP'),
+                       html.H1(children = 'Mental Health Predictor',
+                       style={'textAlign':'center'}),
+                       html.Div(dcc.Dropdown(id='dropdown', options = state_labels)),
+                       dcc.Graph(id='fig1', figure=fig_mental),
+                       dcc.Graph(id='fig2', figure=fig_population),
+                       dcc.Graph(id='fig3', figure=fig_income)])
 
-app = Flask(__name__)
+#UPDATING MENTAL FIGURE
+@app.callback(Output('fig1', 'figure'),
+              Input('dropdown', 'value'))
+def update_mental(state):
+    
+    fig1 = px.scatter(mental_df.loc[mental_df["State_name"]== state].\
+        sort_values('Data_Value'), x="PK", y=["Low_Confidence_Limit", "Data_Value", "High_Confidence_Limit"],
+        labels = {"PK": "City, ST", "value":"Mental Health Score", "Low_Confidence_Limit":"Low Confidence Limit"})
 
-@app.route("/", methods = ["POST", "GET"]) 
-def user_input_info():
-    if request.method == "POST":
-        user_input = request.form["nm"]
-        return render_template("Results.html")
-    else:
-        return render_template("index.html")
+    fig1.update_layout(title = {"text":"Mental Health Score per City","y":0.9,"x":0.5, "xanchor":"center", "yanchor":"top"})
+    return fig1
 
+# UPDATING POPULATION FIGURE
+@app.callback(Output('fig2', 'figure'),
+              Input('dropdown', 'value'))
+def update_population(state):
+    
+    fig2 = px.histogram(population_df.loc[population_df["state_name"]== state].sort_values('population', ascending=False).\
+        head(20), x='PK', y=['population','density'], barmode='group', labels = {"sum of value": "Total", "PK": "City, ST"},title = "Total Population with Calculated Density")
+    fig2.update_layout(title = {"text":"Total Population with Calculated Density","y":0.9,"x":0.5, "xanchor":"center", "yanchor":"top"})
 
-@app.route("/About")
-def about():
-    return render_template("About.html")
+    return fig2
 
-@app.route("/Team")
-def team():
-    return render_template("Team.html")
+# UPDATING INCOME FIGURE
+@app.callback(Output('fig3', 'figure'),
+              Input('dropdown', 'value'))
+def update_income(state):
+    
+    fig3 = px.scatter(income_df.loc[income_df["State_Name"]== state].sort_values('Stdev', ascending=True), x="Median", y="PK",
+	         size="Stdev", color="State_Name",
+                 hover_name="PK", log_x=True, labels = {"Median":"Median Income for City", "PK": "City, ST"},title = "City Median Income Tied to Standard Deviation")
+    fig3.update_layout(title = {"text":"City Median Income Tied to Standard Deviation","y":0.9,"x":0.5, "xanchor":"center", "yanchor":"top"})
+    
+    
+    return fig3
 
-# @app.route("/<name>")
-# def something(name):
-#     return render_template("index.html", content=name, r=2)
-
-if __name__ == "__main__":
-    app.run(debug = True)
-
-
-
-
-
-
-# @app.route('/')
-# def welcome():
-#     return(
-#      '''
-#     Welcome to the Mental Health Predictor! <br/>
-#     Available Routes: <br/>
-#     /mhp_predictor/v1.0/City_Population_Data <br/>
-#     /mhp_predictor/v1.0/Income_Data <br/>
-#     /mhp_predictor/v1.0/Mental_Health_Data <br/>
-#     '''   )
-
-# @app.route("/mhp_predictor/v1.0/City_Population_Data")
-# def cities():
-#     #make the query
-#     city_df_query = session.query(City.city, City.state_id, City.state_name, City.population, City.density).all()
-#     #make the df
-#     city_df = pd.DataFrame(city_df_query, columns=['City','State_ID', 'State_Name', 'Population', 'Density'])
-#         #city_df.set_index(city_df['City'], inplace= True)
-#     #make the json structure?
-#     bc_jsonify = {
-#         city_df['City']:[
-#             {"State ID": city_df['State_ID'],
-#             "State Name": city_df['State_Name'],
-#             "Population": city_df['Population'],
-#             "Density": city_df['Density']}
-#         ]
-#         }
-#     return jsonify(bc_jsonify)
+app.run_server(debug=True)
